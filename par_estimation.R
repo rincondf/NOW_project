e9c998e09778aec26efbc5bdb2f836744ffeb0bb
Diego <diego.rincon@wsu.edu>
2026-04-25 15:57:24 -0700

    mixtools

--- par_estimation.R
+++ par_estimation.R
@@ -30,3 +30,67 @@ points(NOW_pistachio$NOW_DD, NOW_pistachio$moths, col = "red")
 
 plot(NOW_almond$NOW_DD, NOW_almond$pmoths)
 points(NOW_pistachio$NOW_DD, NOW_pistachio$pmoths, col = "red")
+
+
+
+# Almonds
+
+AlmondsDDs <- rep(NOW_almond$NOW_DD, round(NOW_almond$pmoths*1000))
+
+hist(AlmondsDDs, breaks = seq(0, 2100, 15))
+
+
+library(mixtools)
+
+Alm_mix <- weibullRMM_SEM(AlmondsDDs, maxit = 500, k = 4)
+
+
+Alm_mix1 <- gammamixEM(AlmondsDDs, maxit = 500, k = 4)
+
+Alm_mix2 <- normalmixEM(AlmondsDDs, maxit = 500, k = 4)
+
+
+
+
+hist(AlmondsDDs, breaks = seq(0, 2100, 15), freq = FALSE)
+lines(seq(0, 2100, 0.01), dweibull(seq(0, 2100, 0.01), shape = Alm_mix$shape[1],
+                                 scale = Alm_mix$scale[1]), lwd = 2)
+
+lines(seq(0, 2100, 0.01), dweibull(seq(0, 2100, 0.01), shape = Alm_mix$shape[2],
+                                    scale = Alm_mix$scale[2]), lwd = 2)
+
+lines(seq(0, 2100, 0.01), dweibull(seq(0, 2100, 0.01), shape = Alm_mix$shape[3],
+                                   scale = Alm_mix$scale[3]), lwd = 2)
+
+lines(seq(0, 2100, 0.01), dweibull(seq(0, 2100, 0.01), shape = Alm_mix$shape[4],
+                                   scale = Alm_mix$scale[4]), lwd = 2)
+
+hist(AlmondsDDs, breaks = seq(0, 2100, 15), freq = FALSE)
+lines(seq(0, 2000, 0.01), dgamma(seq(0, 2000, 0.01), shape = Alm_mix1$gamma.pars[1, 1],
+                                 scale = Alm_mix1$gamma.pars[2, 1])/2, lwd = 2)
+
+lines(seq(0, 2000, 0.01), dgamma(seq(0, 2000, 0.01), shape = Alm_mix1$gamma.pars[1, 2],
+                                 scale = Alm_mix1$gamma.pars[2, 2])/2)
+
+lines(seq(0, 2000, 0.01), dgamma(seq(0, 2000, 0.01), shape = Alm_mix1$gamma.pars[1, 3],
+                                 scale = Alm_mix1$gamma.pars[2, 3])/2, lwd = 2)
+
+lines(seq(0, 2000, 0.01), dgamma(seq(0, 2000, 0.01), shape = Alm_mix1$gamma.pars[1, 4],
+                                 scale = Alm_mix1$gamma.pars[2, 4])/2)
+
+
+
+
+hist(AlmondsDDs, breaks = seq(0, 2100, 15), freq = FALSE)
+lines(seq(0, 2000, 0.01), dnorm(seq(0, 2000, 0.01), mean = Alm_mix2$mu[1],
+                                 sd = Alm_mix2$sigma[1])/2, lwd = 2)
+
+lines(seq(0, 2000, 0.01), dnorm(seq(0, 2000, 0.01), mean = Alm_mix2$mu[2],
+                                sd = Alm_mix2$sigma[2])/2, lwd = 2)
+
+lines(seq(0, 2000, 0.01), dnorm(seq(0, 2000, 0.01), mean = Alm_mix2$mu[3],
+                                sd = Alm_mix2$sigma[3])/2, lwd = 2)
+
+lines(seq(0, 2000, 0.01), dnorm(seq(0, 2000, 0.01), mean = Alm_mix2$mu[4],
+                                sd = Alm_mix2$sigma[4])/2, lwd = 2)
+
